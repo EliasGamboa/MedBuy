90713a0465c4a84783a682f3d7f0b87b0937ce59
EliasGamboa <sailegamboa@gmail.com>
2020-11-09 12:31:15 -0600

    first commit

---
+++ MedBuy.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MedBuy.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ MedBuy.Infraestructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MedBuy.Infraestructure
+{
+    public class Class1
+    {
+    }
+}
